37e6d4e0d0f19f770afe0edf17351b5752848cae
Christian Martinez <christian@mixdlabs.com>
2021-02-09 10:51:16 -0700

    Readme

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 ## What is this? 
 
-Create stunning text fill animations with the power of javascript. Use colorfill on links, titles, or randomly throughout your page to catch the users eye.
+Colorfilljs is a lightweight text fill animation tool using the power of javascript. It automatically generates your css on whatever pages you wish to include it. Use colorfill on links, titles, or randomly throughout your web app to make your text stand out.
 
 ## Installation
 
-` npm i colorfill.js --save `
+` npm i colorfilljs --save `
 
 Then...
 
 ```
-import { colorfillJs } from 'colorfill.js';
+import { colorfilljs } from 'colorfilljs';
 
-colorfillJs ({
-    color: 'default'
+colorfilljs({
+  textColor: '#27285D',
+  fillColor: '#D13D78',
+  cursor: 'pointer',
+  positionOne: 'translateY(100%)',
+  positionTwo: 'translateY(-100%)',
+  positionThree: 'translateY(0)',
+  duration: '1s',
+  timing: 'ease-in-out'
 })
 ```
+or set as default
+
+```
+colorfilljs('')
+```
+
+# Configuration
+You can configure your own colorfill at the colorfill [codepen](https://codepen.io/christianmartinez/pen/KKgyywR)
+
+Colorfill takes a maximum of 8 arguments (all mentioned in the above example). If any are left blank, it will use defaults.
+
--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-function colorfillJs(options) {
-    let text = document.querySelectorAll('.text');
+function colorfilljs(options) {
 
-    if (options.color === 'default') {
-        options.color = 'red';
-    } else {
-        options.color = options.color;
-    }
+    const style = document.createElement('style');
 
-    text.forEach(elem => {
-        elem.style.color = options.color;
-    })
+    style.innerHTML = `     
+   .colorfill-text {
+     color: ${options.textColor ? options.textColor : '#D9269A'};
+     cursor: ${options.cursor ? options.cursor : 'default'};
+     width: 100%;
+   }
+   
+   span.colorfill {
+     position: absolute;
+     overflow: hidden;
+     transform:  ${options.positionOne ? options.positionOne : 'translateX(-100%)'};
+     transition: transform ${options.duration ? options.duration : '0.75s'} ${options.timing ? options.timing : 'ease-in-out'};
+   }
+   
+   h1:hover span.colorfill, h2:hover span.colorfill, h3:hover span.colorfill,
+   h4:hover span.colorfill, h5:hover span.colorfill, h6:hover span.colorfill,
+   a:hover span.colorfill, p:hover span.colorfill {
+     transform: ${options.positionThree ? options.positionThree : 'translateX(0)'};   
+   }
+   
+   span.colorfill::before { 
+     display: inline-block;
+     color: ${options.fillColor ? options.fillColor : '#161A3C'};
+     content: attr(data-content);
+     transform: ${options.positionTwo ? options.positionTwo : 'translateX(100%)'};
+     transition: ${options.duration ? options.duration : '0.75s'} ${options.timing ? options.timing : 'ease-in-out'};
+   }
+   
+   h1:hover span.colorfill::before, h2:hover span.colorfill::before, h3:hover span.colorfill::before,
+   h4:hover span.colorfill::before, h5:hover span.colorfill::before, h6:hover span.colorfill::before,
+   a:hover span.colorfill::before, p:hover span.colorfill::before {
+     transform: ${options.positionThree ? options.positionThree : 'translateX(0)'};   
+   }`;
+
+    document.getElementsByTagName('head')[0].appendChild(style);
 }
 
-module.exports.colorfillJs = colorfillJs;
+module.exports.colorfilljs = colorfilljs;
